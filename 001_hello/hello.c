4a2ab8c11142856812e9973eedd508e4de326cab
Fabio Z Tessitore <fabioztessitore@gmail.com>
2019-09-13 10:02:57 +0200

    hello

---
+++ 001_hello/hello.c
@@ -0,0 +1,15 @@
+/* codice 001 */
+
+/* Stampa un saluto */
+
+/* Compilare da terminale Linux */
+
+/* NON e' una libreria, e' un file di intestazione */
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+
+    return 0;
+}
