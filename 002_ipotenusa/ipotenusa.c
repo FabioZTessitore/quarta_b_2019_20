fdb32aedfca01050a35704d835e45fe84d9cdcac
Fabio Z Tessitore <fabioztessitore@gmail.com>
2019-09-13 10:28:34 +0200

    work in progress

---
+++ 002_ipotenusa/ipotenusa.c
@@ -0,0 +1,25 @@
+/* Codice 002 */
+
+/* Calcola l'ipotenusa di un triangolo rettangolo */
+
+/* Utilizzo della libreria matematica */
+
+#include <stdio.h>
+
+double ipotenusa(double cateto1, double cateto2);
+
+int main()
+{
+    double risultato;
+
+    risultato = ipotenusa(3., 4.);
+
+    printf("Risultato: %lf", risultato);
+
+    return 0;
+}
+
+double ipotenusa(double cateto1, double cateto2)
+{
+    return ...;
+}
