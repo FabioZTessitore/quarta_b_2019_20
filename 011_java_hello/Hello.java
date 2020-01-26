9e57d2bb81dbac007f129f03f8537bb96219b5f9
Fabio Z Tessitore <fabioztessitore@gmail.com>
2020-01-26 10:12:51 +0100

    intro java

---
+++ 011_java_hello/Hello.java
@@ -0,0 +1,14 @@
+// Hello.java
+
+// Stampa "Hello World!"
+
+/* Compilazione ed esecuzione:
+$ javac Hello.java
+$ java Hello
+*/
+
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
